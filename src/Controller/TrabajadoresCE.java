c0432d0f8afcc2d59b9c3de4cb6dfbf9e640028c
themo <themo@Lodic>
2022-11-04 20:44:30 -0500

    Clase de prubea controler

---
+++ src/Controller/TrabajadoresCE.java
@@ -0,0 +1,26 @@
+
+package Controller;
+
+import java.util.logging.Logger;
+
+
+public class TrabajadoresCE {
+    private String prueba;
+
+    public String getPrueba() {
+        return prueba;
+    }
+
+    public void setPrueba(String prueba) {
+        this.prueba = prueba;
+    }
+
+    public TrabajadoresCE(String prueba) {
+        this.prueba = prueba;
+    }
+    
+    
+    
+    
+    
+}
